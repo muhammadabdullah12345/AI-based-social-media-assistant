b1c71063f90737ab65a9e6df333d141f7d99e1e6
Muhammad Abdullah <abd0172817@gmail.com>
2026-01-27 00:33:56 +0500

    chore: update @prisma/client to version 7.3.0 and add prisma as a dependency
    refactor: remove prisma.config.ts and migrate to new schema structure
    
    fix: update signup route to hash passwords before storing in the database
    
    refactor: change global variable for PrismaClient to use globalThis
    
    chore: recreate migration files and schema.prisma for database setup

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -2,17 +2,17 @@ import { prisma } from "@/src/lib/prisma";
 import bcrypt from "bcryptjs";
 
 export async function POST(req: Request) {
-  console.log("❤ Signup API called");
+  // console.log("❤ Signup API called");
   const { email, password } = await req.json();
-  console.log("❤ Signup credentials recieved");
+  // console.log("❤ Signup credentials recieved");
   const hashedPassword = await bcrypt.hash(password, 12);
-  console.log("❤ Signup password hashed");
+  // console.log("❤ Signup password hashed");
   const user = await prisma.user.create({
     data: {
       email,
-      password,
+      password: hashedPassword,
     },
   });
-  console.log("❤ Signup user created");
+  // console.log("❤ Signup user created");
   return Response.json({ success: true, message: "User created successfully" });
 }
